3f36c8999a0a4b72bd19d7587b5f3afb011ac131
Credrido <kpunoff@gmail.com>
2024-05-18 19:33:52 +0300

    Second

---
+++ Models/BlogEntity.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace _4._4.ViewModels;
+
+public class BlogEntity
+{
+    public string Article {get; set;}
+
+    public string Text {get; set;}
+
+    public string ImagePath {get; set;}
+
+    public List<String> Tags {get; set;}
+}
---
+++ Models/NewsEntity.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace _4._4.ViewModels;
+
+public class NewsEntity
+{
+    public string Text { get; set; }
+
+    public DateTime PublishDate { get; set; }
+}
