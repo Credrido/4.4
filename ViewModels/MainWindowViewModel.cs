68694243895c897893a0243b437b519102ca7904
Credrido <kpunoff@gmail.com>
2024-05-18 18:42:17 +0300

    Commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._4.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace _4._4.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+    
+}
